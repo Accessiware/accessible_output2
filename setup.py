9fb389816a56c00cdb4c6b5d4bd32082d4a95c29
NicklasMCHD <NicklasMCHD@live.dk>
2019-02-17 13:31:14 +0100

    Initial code commit

---
+++ setup.py
@@ -0,0 +1,50 @@
+from setuptools import setup, find_packages
+import io
+from platform import system
+
+_system = system()
+
+install_requires = ['platform_utils']
+if _system == 'Windows':
+	install_requires += [
+	'pypiwin32',
+	'libloader'
+]
+elif _system == 'Darwin':
+	install_requires += [
+		'appscript',
+	]
+
+dependency_links = [
+	'http://github.com/accessiware/libloader/tarball/master#egg=libloader-0.21',
+	'http://github.com/accessiware/platform_utils/tarball/master#egg=platform_utils-0.40'
+]
+
+__doc__ = 'Library to provide speech and braille output to a variety of different screen readers and other accessibility solutions.',
+
+with io.open('readme.rst', encoding='UTF8') as readme:
+	long_description = readme.read()
+
+setup(
+	name = 'accessible_output2',
+	author = 'Tyler Spivey',
+	author_email = 'tspivey@pcdesk.net',
+	version='0.13',
+	description = __doc__,
+	long_description = long_description,
+	package_dir = {'accessible_output2': 'accessible_output2'},
+	packages = find_packages(),
+	package_data = {"accessible_output2": ["lib/*"]},
+	zip_safe = False,
+	classifiers = [
+		'Development Status :: 5 - Stable',
+		'Intended Audience :: Developers',
+		'Operating System :: Microsoft :: Windows',
+		'Programming Language :: Python',
+		'License :: OSI Approved :: MIT License',
+'Topic :: Adaptive Technologies',
+'Topic :: Software Development :: Libraries'
+],
+	install_requires = install_requires,
+dependency_links=dependency_links,
+	)
